d848bd536d9319f5ff0b37cb08fe6513103b80e3
Artur <artur131996@gmail.com>
2022-08-02 17:26:23 +0300

    Added score counter

--- Apple Picker Prototype/Assets/Scripts/Basket.cs
+++ Apple Picker Prototype/Assets/Scripts/Basket.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
     void Start()
     {
-        
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 mousePos2D = Input.mousePosition;
@@ -28,6 +32,11 @@ public class Basket : MonoBehaviour
         if (collidedWith.tag == "Apple")
         {
             Destroy(collidedWith);
+
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
         }
+        
     }
 }
